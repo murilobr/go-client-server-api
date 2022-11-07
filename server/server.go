007b7ab6595474c72b493f079049a0fc8d116c75
Murilo da Silva <murilobr@gmail.com>
2022-11-06 22:42:37 -0300

    feat: implements context timeout to endpoint request and database save

--- server/server.go
+++ server/server.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type USDBRL struct {
@@ -36,9 +41,21 @@ func main() {
 }
 
 func GetExchangeHandler(w http.ResponseWriter, r *http.Request) {
-	res, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+	ctxReq := context.Background()
+	ctxReq, cancelCtxReq := context.WithTimeout(ctxReq, time.Millisecond*10)
+	defer cancelCtxReq()
+
+	req, err := http.NewRequestWithContext(ctxReq, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Create new request error: %v\n", err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Request error: %v\n", err)
+		w.WriteHeader(http.StatusRequestTimeout)
+		w.Write([]byte(""))
+		return
 	}
 	defer res.Body.Close()
 
@@ -59,4 +76,35 @@ func GetExchangeHandler(w http.ResponseWriter, r *http.Request) {
 	cotacao := Cotacao{Value: data.USDBRL.Bid}
 
 	json.NewEncoder(w).Encode(cotacao)
+
+	// ----------------------------------------------------------------
+	// SAVE TO DATABASE
+
+	ctxDb := context.Background()
+	ctxDb, cancelCtxDb := context.WithTimeout(ctxDb, time.Millisecond*10)
+	defer cancelCtxDb()
+
+	db, err := sql.Open("sqlite3", "cotacao.db")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot connect to database error: %v\n", err)
+	}
+	defer db.Close()
+
+	createCotacaoTable := `CREATE TABLE cotacao (id integer NOT NULL PRIMARY KEY AUTOINCREMENT, value TEXT);`
+
+	stmt, err := db.Prepare(createCotacaoTable)
+	if err == nil {
+		stmt.Exec()
+	}
+
+	insertCotacaoSQL := `INSERT INTO cotacao(value) VALUES (?)`
+	stmt, err = db.Prepare(insertCotacaoSQL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot create statement to insert into table cotacao error: %v\n", err)
+	}
+
+	_, err = stmt.ExecContext(ctxDb, cotacao.Value)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot insert into table cotacao error: %v\n", err)
+	}
 }
